16b6cf029df763b59a4175cc352d178330735aeb
Dipang-bhadrecha <dipaang.bhadrecha@gmail.com>
2025-08-24 22:54:28 +0530

    auth folder added

--- promptlearn/src/App.jsx
+++ promptlearn/src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react';
 import PromptArea from "./component/promptArea/promptArea";
 import "./styles/promptArea.css";
 import Sidebar from "./component/Sidebar/Sidebar";
-import "./styles/sidebar.css"; 
+import "./styles/sidebar.css";
+import CanvasBoard from './component/canvas/canvas';
 
 export default function App() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
@@ -10,7 +11,8 @@ export default function App() {
   return (
     <div style={{ display: 'flex', height: '100vh', overflow: 'hidden' }}>
       {isSidebarOpen && <Sidebar />}
-      <PromptArea isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} />
+      {/* <PromptArea isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} /> */}
+      <CanvasBoard />
     </div>
   );
 }
