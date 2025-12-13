ef169b5520c10cf73c1856c8993107b319c7898a
Dipang-bhadrecha <dipaang.bhadrecha@gmail.com>
2025-12-13 19:03:15 +0530

    vercel version match with local

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,6 @@ const nextConfig: NextConfig = {
   experimental: {
     // leave empty unless you need Turbo or React Compiler
   },
-  eslint: {
-    // ✅ prevents build from failing on ESLint errors in Vercel
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
